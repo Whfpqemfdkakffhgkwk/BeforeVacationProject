2edc482506baf0ff18655aebdbcfa30033e4c51e
qkrgustn <sdh210212@sdh.hs.kr>
2022-07-15 08:50:07 +0900

    점프기능

---
+++ BeforeGameJam/Assets/Hyeonsu/Scripts/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+    private int jumpCount = 2;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject coll = collision.gameObject;
+        switch(coll.tag)
+        {
+            case "Floor":
+                jumpCount = 2;
+                break;
+            case "Enemy":
+                //transform.DO
+                break;
+        }
+    }
+    void Jump()
+    {
+        if(jumpCount > 0)
+        {
+            rb.velocity = new Vector2(0f, 0f);
+            rb.AddForce(Vector2.up * 1000);
+            jumpCount--;
+        }
+    }
+}
