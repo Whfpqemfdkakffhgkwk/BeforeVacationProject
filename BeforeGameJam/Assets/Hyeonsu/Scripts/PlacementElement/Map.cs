b496f0f888afece8c455051262a6141c5850d137
qkrgustn <sdh210212@sdh.hs.kr>
2022-07-18 01:49:59 +0900

    맵관련 스크립트 통일
    여기다가 맵 반복 코드 짜면 됨

--- BeforeGameJam/Assets/Hyeonsu/Scripts/PlacementElement/Map.cs
+++ BeforeGameJam/Assets/Hyeonsu/Scripts/PlacementElement/Map.cs
@@ -6,6 +6,20 @@ public class Map : MonoBehaviour
 {
     void Update()
     {
-        transform.position += Vector3.left * Time.deltaTime * Player.Instance.speed;
+        string Type = gameObject.tag;
+        switch (Type)
+        {
+            case "Map":
+                transform.position += Vector3.left * Time.deltaTime * Player.Instance.speed;
+                break;
+            case "Background":
+                transform.localPosition += Vector3.left * Time.deltaTime * (Player.Instance.speed / 3);
+                //배경 반복 코드
+                break;
+            case "Floor":
+                //바닥 반복 코드
+                break;
+        }
     }
+
 }
