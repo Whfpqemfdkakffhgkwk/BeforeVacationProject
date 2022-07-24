fd1674bfa331c7a249bbaee21309fb4a6847fa7c
dleodns1591 <88140817+dleodns1591@users.noreply.github.com>
2022-07-24 15:20:43 +0900

    스테이지 선택 창에서 상점으로 넘어 갈 수 있으며, 설정창을 열 수 있음
    스테이지 선택 창에서 상점으로 넘어 갈 수 있으며, 설정창을 열 수 있음

--- BeforeGameJam/Assets/LeeDaeWoon/Script/Manager/StagePick_Manager.cs
+++ BeforeGameJam/Assets/LeeDaeWoon/Script/Manager/StagePick_Manager.cs
@@ -15,11 +15,14 @@ public class StagePick_Manager : MonoBehaviour
 
     [Header("메뉴")]
     public GameObject Menu;
+
+    public Image Touch_Limit;
+    public GameObject Setting_Window;
     private bool Open_Check;
 
     void Start()
     {
-
+        Touch_Limit.raycastTarget = false;
     }
 
     void Update()
@@ -70,10 +73,30 @@ public class StagePick_Manager : MonoBehaviour
 
     #endregion
 
-    public void Menu_Click()
+    public void Menu_Click() => StartCoroutine(MenuOpen());
+    public void Shop_Clikck() => SceneManager.LoadScene("Shop");
+
+    #region 설정 버튼
+    public void Setting_Click() => SettingWindow_Open();
+
+    public void Setting_Close() => StartCoroutine(SettingWindow_Close());
+
+    #region 창 연출
+    IEnumerator SettingWindow_Close()
     {
-        StartCoroutine(MenuOpen());
+        Setting_Window.transform.DOScale(new Vector3(0, 0, 0), 1f).SetEase(Ease.InBack);
+        yield return new WaitForSeconds(1f);
+        Touch_Limit.raycastTarget = false;
     }
+
+    void SettingWindow_Open()
+    {
+        Touch_Limit.raycastTarget = true;
+        Setting_Window.transform.DOScale(new Vector3(1, 1, 1), 1f).SetEase(Ease.OutBack);
+    }
+    #endregion
+
+    #endregion
 
     public IEnumerator MenuOpen()
     {
